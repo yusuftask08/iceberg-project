415f45d04e5db0f6347faec4bbe924df31770dd3
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-12-11 21:55:18 +0300

    AppointmentsCreate : Form added

--- src/views/App/AppointmentsCreate.vue
+++ src/views/App/AppointmentsCreate.vue
@@ -5,11 +5,41 @@
         <h2 class="h2">Randevu oluştur</h2>
       </v-toolbar>
     </div>
+    <div class="mx-5">
+      <v-col class="d-flex justify-center">
+        <v-text-field
+          v-model="name"
+          class="mr-2"
+          outlined
+          label="Müşteri İsim"
+        />
+        <v-text-field v-model="surname" outlined label="Müşteri Soyisim" />
+      </v-col>
+      <v-col class="d-flex justify-center">
+        <v-text-field
+          v-model="email"
+          class="mr-2"
+          outlined
+          label="Müşteri Email"
+        />
+        <v-text-field v-model="tel" outlined label="Müşteri Telefon" />
+      </v-col>
+    </div>
+    <div class="d-flex justify-center">Map</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      surname: "",
+      email: "",
+      tel: "",
+    };
+  },
+};
 </script>
 
 <style>
